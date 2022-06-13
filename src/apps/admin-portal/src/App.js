78605040902156cbcf8a7396c921d2589d440324
Dhananjeyan Balaretnaraja <sdbala@gmail.com>
2022-06-13 07:23:25 +0530

    added functionality to admin portal to test cors issue

--- src/apps/admin-portal/src/App.js
+++ src/apps/admin-portal/src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
+  const [caption, setCaption] = useState("");
+
+  const callFaculties = () => {
+    fetch("http://localhost:5001/departments")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setCaption(JSON.stringify(result));
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  };
   return (
     <div>
       <h1>Admin Portal</h1>
+
+      <div>{caption}</div>
+
+      <div>
+        <button type="button" onClick={callFaculties}>
+          Call Faculties
+        </button>
+      </div>
     </div>
   );
 }
--- src/services/faculties/index.js
+++ src/services/faculties/index.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 app.get("/", (request, response) => {
   response.send("Faculties service GET request");
+});
+
+app.get("/faculties", (request, response) => {
+  response.send("Get the list of all the faculties");
+});
+
+app.get("/departments", (request, response) => {
+  response.json([
+    {
+      id: 1,
+      name: "Medicine",
+    },
+    {
+      id: 2,
+      name: "Computer Science",
+    },
+    {
+      id: 1,
+      name: "Agriculture",
+    },
+  ]);
+});
+
+app.get("/departments/count", (request, response) => {
+  response.send("Departments in a faculty count");
+});
+
+app.get("/departments/count/something", (request, response) => {
+  response.send("Departments in a faculty count");
 });
 
 app.listen(5001, () => console.log("Faculties Server is up and running"));
